e6331f5b33c0219f29e4024e004246511c70a804
Nxirda <Nxirda@users.noreply.github.com>
2025-06-25 21:54:37 +0200

    Return same type as Sylbe branch for overload resolution

--- test/algorithm/algos/context/eve/find.cpp
+++ test/algorithm/algos/context/eve/find.cpp
@@ -20,7 +20,7 @@
 #include <kwk/context/cpu/context.hpp>
 */
 
-TTS_CASE("Check for kwk::find(kwk::cpu, In, value) 1D")
+TTS_CASE("Check for kwk::find(kwk::simd, In, value) 1D")
 {
   // Empty array
   {
@@ -38,34 +38,34 @@ TTS_CASE("Check for kwk::find(kwk::cpu, In, value) 1D")
 
   // Not found
   {
-    auto res = kwk::find(kwk::cpu, view, 11);
+    auto res = kwk::find(kwk::simd, view, 11);
     TTS_EQUAL(res.has_value(), false);
   }
 
   // First position
   {
-    auto res = kwk::find(kwk::cpu, view, -10);
+    auto res = kwk::find(kwk::simd, view, -10);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{0});
     else                 TTS_FAIL("find returned std::nullopt and not the expected valid value.");
   }
 
   // Random position
   {
-    auto res = kwk::find(kwk::cpu, view, 12);
+    auto res = kwk::find(kwk::simd, view, 12);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{11});
     else                 TTS_FAIL("find returned std::nullopt and not the expected valid value.");
   }
 
   // Last position
   {
-    auto res = kwk::find(kwk::cpu, view, (static_cast<int>(input_size) - 1) * 2 - 10);
+    auto res = kwk::find(kwk::simd, view, (static_cast<int>(input_size) - 1) * 2 - 10);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{input_size-1});
     else                 TTS_FAIL("find returned std::nullopt and not the expected valid value.");
   }
 };
 
 
-TTS_CASE("Check for kwk::find_if(kwk::cpu, In, func) 1D")
+TTS_CASE("Check for kwk::find_if(kwk::simd, In, func) 1D")
 {
   // Empty array
   {
@@ -73,7 +73,7 @@ TTS_CASE("Check for kwk::find_if(kwk::cpu, In, func) 1D")
     std::vector<int> input(input_size);
     auto view = kwk::view{kwk::source = input, kwk::of_size(input_size)};
     auto func = [&](auto item) { return (item % 2) == 0; };
-    auto res  = kwk::find_if(kwk::cpu, view, func);
+    auto res  = kwk::find_if(kwk::simd, view, func);
     TTS_EQUAL(res.has_value(), false);
   }
 
@@ -88,21 +88,21 @@ TTS_CASE("Check for kwk::find_if(kwk::cpu, In, func) 1D")
 
   // First element should be valid
   {
-    auto res = kwk::find_if(kwk::cpu, view, func_even);
+    auto res = kwk::find_if(kwk::simd, view, func_even);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{0});
     else                 TTS_FAIL("find_if returned std::nullopt and not the expected valid value.");
   }
 
   // Odd item not found
   {
-    auto res = kwk::find_if(kwk::cpu, view, func_odd);
+    auto res = kwk::find_if(kwk::simd, view, func_odd);
     TTS_EQUAL(res.has_value(), false);
   }
 
   // Added matching item (odd number) at random position : 12
   input[12] = 7;
   {
-    auto res = kwk::find_if(kwk::cpu, view, func_odd);
+    auto res = kwk::find_if(kwk::simd, view, func_odd);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{12});
     else                 TTS_FAIL("find_if returned std::nullopt and not the expected valid value.");
   }
@@ -111,7 +111,7 @@ TTS_CASE("Check for kwk::find_if(kwk::cpu, In, func) 1D")
   input[12] = 24;
   input[0] = 7;
   {
-    auto res = kwk::find_if(kwk::cpu, view, func_odd);
+    auto res = kwk::find_if(kwk::simd, view, func_odd);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{0});
     else                 TTS_FAIL("find_if returned std::nullopt and not the expected valid value.");
   }
@@ -120,13 +120,13 @@ TTS_CASE("Check for kwk::find_if(kwk::cpu, In, func) 1D")
   input[0] = 0;
   input[input_size-1] = 7;
   {
-    auto res = kwk::find_if(kwk::cpu, view, func_odd);
+    auto res = kwk::find_if(kwk::simd, view, func_odd);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{input_size-1});
     else                 TTS_FAIL("find_if returned std::nullopt and not the expected valid value.");
   }
 };
 
-TTS_CASE("Check for kwk::find_if_not(kwk::cpu, In, func) 1D")
+TTS_CASE("Check for kwk::find_if_not(kwk::simd, In, func) 1D")
 {
   // Empty array
   {
@@ -135,7 +135,7 @@ TTS_CASE("Check for kwk::find_if_not(kwk::cpu, In, func) 1D")
     auto view = kwk::view{kwk::source = input, kwk::of_size(input_size)};
 
     auto func = [&](auto item) { return (item % 2) == 0; };
-    auto res = kwk::find_if_not(kwk::cpu, view, func);
+    auto res = kwk::find_if_not(kwk::simd, view, func);
 
     TTS_EQUAL(res.has_value(), false);
   }
@@ -149,7 +149,7 @@ TTS_CASE("Check for kwk::find_if_not(kwk::cpu, In, func) 1D")
   // Looking for the first item that is not odd
   {
     auto func_odd = [&](auto item) { return (item % 2) == 1; };
-    auto res = kwk::find_if_not(kwk::cpu, view, func_odd);
+    auto res = kwk::find_if_not(kwk::simd, view, func_odd);
 
     // To avoid a nasty compiler error
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{0});
@@ -160,14 +160,14 @@ TTS_CASE("Check for kwk::find_if_not(kwk::cpu, In, func) 1D")
   auto func_even = [&](auto item) { return (item % 2) == 0; };
   {
     // "not even" item not found
-    auto res = kwk::find_if_not(kwk::cpu, view, func_even);
+    auto res = kwk::find_if_not(kwk::simd, view, func_even);
     TTS_EQUAL(res.has_value(), false);
   }
 
   // Added matching item (odd number) at a random position
   input[12] = 7;
   {
-    auto res = kwk::find_if_not(kwk::cpu, view, func_even);
+    auto res = kwk::find_if_not(kwk::simd, view, func_even);
     // To avoid a nasty compiler error
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{12});
     else                 TTS_FAIL("find_if_not returned std::nullopt and not the expected valid value.");
@@ -177,7 +177,7 @@ TTS_CASE("Check for kwk::find_if_not(kwk::cpu, In, func) 1D")
   input[12] = 24;
   input[0] = 3;
   {
-    auto res = kwk::find_if_not(kwk::cpu, view, func_even);
+    auto res = kwk::find_if_not(kwk::simd, view, func_even);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{0});
     else                 TTS_FAIL("find_if_not returned std::nullopt and not the expected valid value.");
   }
@@ -186,7 +186,7 @@ TTS_CASE("Check for kwk::find_if_not(kwk::cpu, In, func) 1D")
   input[0] = 0;
   input[input_size-1] = 3;
   {
-    auto res = kwk::find_if_not(kwk::cpu, view, func_even);
+    auto res = kwk::find_if_not(kwk::simd, view, func_even);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{input_size-1});
     else                 TTS_FAIL("find_if_not returned std::nullopt and not the expected valid value.");
   }
@@ -194,7 +194,7 @@ TTS_CASE("Check for kwk::find_if_not(kwk::cpu, In, func) 1D")
 
 
 
-TTS_CASE("Check for kwk::find_last(kwk::cpu, In, value) 1D")
+TTS_CASE("Check for kwk::find_last(kwk::simd, In, value) 1D")
 {
   // // Empty array
   {
@@ -202,7 +202,7 @@ TTS_CASE("Check for kwk::find_last(kwk::cpu, In, value) 1D")
     std::vector<int> input(input_size);
     auto view = kwk::view{kwk::source = input, kwk::of_size(input_size)};
     int search{6};
-    auto res = kwk::find_last(kwk::cpu, view, search);
+    auto res = kwk::find_last(kwk::simd, view, search);
     TTS_EQUAL(res.has_value(), false);
   }
 
@@ -213,13 +213,13 @@ TTS_CASE("Check for kwk::find_last(kwk::cpu, In, value) 1D")
 
   // Not found
   {
-    auto res = kwk::find_last(kwk::cpu, view, 8);
+    auto res = kwk::find_last(kwk::simd, view, 8);
     TTS_EQUAL(res.has_value(), false);
   }
   
   // Last position
   {
-    auto res = kwk::find_last(kwk::cpu, view, 6);
+    auto res = kwk::find_last(kwk::simd, view, 6);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{input_size-1});
     else                 TTS_FAIL("find_last returned std::nullopt and not the expected valid value.");
   }
@@ -227,7 +227,7 @@ TTS_CASE("Check for kwk::find_last(kwk::cpu, In, value) 1D")
   // Random position
   input[4] = 8;
   {
-    auto res = kwk::find_last(kwk::cpu, view, 8);
+    auto res = kwk::find_last(kwk::simd, view, 8);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{4});
     else                 TTS_FAIL("find_last returned std::nullopt and not the expected valid value.");
   }
@@ -236,13 +236,13 @@ TTS_CASE("Check for kwk::find_last(kwk::cpu, In, value) 1D")
   // First position
   input[0] = 8;
   {
-    auto res = kwk::find_last(kwk::cpu, view, 8);
+    auto res = kwk::find_last(kwk::simd, view, 8);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{0});
     else                 TTS_FAIL("find_last returned std::nullopt and not the expected valid value.");
   }
 };
 
-TTS_CASE("Check for kwk::find_last_if(kwk::cpu, In, func) 1D")
+TTS_CASE("Check for kwk::find_last_if(kwk::simd, In, func) 1D")
 {
   auto func = [](auto const& e) { return (e % 2) == 0; };
 
@@ -251,7 +251,7 @@ TTS_CASE("Check for kwk::find_last_if(kwk::cpu, In, func) 1D")
     const std::size_t input_size = 0;
     std::vector<int> input(input_size);
     auto view = kwk::view{kwk::source = input, kwk::of_size(input_size)};
-    auto res = kwk::find_last_if(kwk::cpu, view, func);
+    auto res = kwk::find_last_if(kwk::simd, view, func);
     TTS_EQUAL(res.has_value(), false);
   }
 
@@ -262,14 +262,14 @@ TTS_CASE("Check for kwk::find_last_if(kwk::cpu, In, func) 1D")
 
   // Not found
   {
-    auto res = kwk::find_last_if(kwk::cpu, view, func);
+    auto res = kwk::find_last_if(kwk::simd, view, func);
     TTS_EQUAL(res.has_value(), false);
   }
 
   // Last position
   input[input_size-1] = 8;
   {
-    auto res = kwk::find_last_if(kwk::cpu, view, func);
+    auto res = kwk::find_last_if(kwk::simd, view, func);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{input_size-1});
     else                 TTS_FAIL("find_last_if returned std::nullopt and not the expected valid value.");
   }
@@ -278,7 +278,7 @@ TTS_CASE("Check for kwk::find_last_if(kwk::cpu, In, func) 1D")
   // Random position
   input[4] = 16;
   {
-    auto res = kwk::find_last_if(kwk::cpu, view, func);
+    auto res = kwk::find_last_if(kwk::simd, view, func);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{4});
     else                 TTS_FAIL("find_last_if returned std::nullopt and not the expected valid value.");
   }
@@ -287,13 +287,13 @@ TTS_CASE("Check for kwk::find_last_if(kwk::cpu, In, func) 1D")
   // First position
   input[0] = 6;
   {
-    auto res = kwk::find_last_if(kwk::cpu, view, func);
+    auto res = kwk::find_last_if(kwk::simd, view, func);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{0});
     else                 TTS_FAIL("find_last_if returned std::nullopt and not the expected valid value.");
   }
 };
 
-TTS_CASE("Check for kwk::find_last_if_not(kwk::cpu, In, func) 1D")
+TTS_CASE("Check for kwk::find_last_if_not(kwk::simd, In, func) 1D")
 {
 
   auto func = [](auto const& e) { return (e % 2) != 0; };
@@ -303,7 +303,7 @@ TTS_CASE("Check for kwk::find_last_if_not(kwk::cpu, In, func) 1D")
     const std::size_t input_size = 0;
     std::vector<int> input(input_size);
     auto view = kwk::view{kwk::source = input, kwk::of_size(input_size)};
-    auto res = kwk::find_last_if_not(kwk::cpu, view, func);
+    auto res = kwk::find_last_if_not(kwk::simd, view, func);
     TTS_EQUAL(res.has_value(), false);
   }
 
@@ -314,14 +314,14 @@ TTS_CASE("Check for kwk::find_last_if_not(kwk::cpu, In, func) 1D")
 
   // Not found
   {
-    auto res = kwk::find_last_if_not(kwk::cpu, view, func);
+    auto res = kwk::find_last_if_not(kwk::simd, view, func);
     TTS_EQUAL(res.has_value(), false);
   }
 
   // Last position
   input[input_size-1] = 8;
   {
-    auto res = kwk::find_last_if_not(kwk::cpu, view, func);
+    auto res = kwk::find_last_if_not(kwk::simd, view, func);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{input_size-1});
     else                 TTS_FAIL("find_last_if_not returned std::nullopt and not the expected valid value.");
   }
@@ -330,7 +330,7 @@ TTS_CASE("Check for kwk::find_last_if_not(kwk::cpu, In, func) 1D")
   // Random position
   input[4] = 16;
   {
-    auto res = kwk::find_last_if_not(kwk::cpu, view, func);
+    auto res = kwk::find_last_if_not(kwk::simd, view, func);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{4});
     else                 TTS_FAIL("find_last_if_not returned std::nullopt and not the expected valid value.");
   }
@@ -339,7 +339,7 @@ TTS_CASE("Check for kwk::find_last_if_not(kwk::cpu, In, func) 1D")
   // First position
   input[0] = 6;
   {
-    auto res = kwk::find_last_if_not(kwk::cpu, view, func);
+    auto res = kwk::find_last_if_not(kwk::simd, view, func);
     if (res.has_value()) TTS_EQUAL(res.value(), kwk::position<1>{0});
     else                 TTS_FAIL("find_last_if_not returned std::nullopt and not the expected valid value.");
   }
